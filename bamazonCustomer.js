2b60909377b1d4034e8d42e64d68b201163ddfd0
origami unicorn <rvwright@gmail.com>
2020-01-10 21:46:02 -0800

    Figured out how to compare an imput value to the sql database values to see if a purchase request was asking for more inventory than was available.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,37 +16,7 @@ connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     listInventory();
-    // start();
 });
-
-function start() {
-    inquirer
-        .prompt([
-            {
-                type: "list",
-                message: "Would you like to [POST] an auction or [BID] on an auction?",
-                choices: ["POST", "BID"],
-                name: "initAction"
-            }
-        ]).then(function (response) {
-
-            console.log(response.initAction);
-            var newAction = response.initAction;
-
-            switch (newAction) {
-                case "POST":
-
-                    postAuction();
-
-                    break;
-                case "BID":
-                    break;
-                case "EXIT":
-                    break;
-            }
-
-        });
-};
 
 // function postAuction() {
 
@@ -109,15 +79,50 @@ function listInventory() {
             inventoryObj.price = results[i].price;
             inventoryArr.push(inventoryObj);
         }
-        return printInventory(inventoryArr);
+        printInventory(inventoryArr);
+        return start();
     })
-}
+};
 
 function printInventory(inventory) {
     for (let i = 0; i < inventory.length; i++) {
-        console.log(`${inventory[i].id}) ${inventory[i].product_name} - ${inventory[i].price}`);
+        console.log(`${inventory[i].id} | ${inventory[i].product_name} - ${inventory[i].price}`);
     }
-}
+};
+
+function start() {
+    inquirer
+        .prompt([
+            {
+                type: "number",
+                message: "Input the ID of the product which you would like to purchase:",
+                name: "productID"
+            },
+            {
+                type: "number",
+                message: "Input the number of units you would like to purchase:",
+                name: "unitsGet"
+            }
+        ]).then(function (response) {
+
+            console.log(response.productID);
+            console.log(response.unitsGet);
+            let checkInv = response.productID;
+            let changeInv = response.unitsGet;
+
+            connection.query("SELECT item_id,stock FROM products WHERE item_id LIKE " + checkInv, function (err, res) {
+                console.log(changeInv);
+                console.log(res[0].stock);
+                if (err) throw err;
+                if (changeInv > parseInt(res[0].stock)) {
+                    console.log("Insufficient quantity.")
+                } else {
+                    console.log(res);
+                }
+            })
+
+        });
+};
 
 // function makeAuctionList() {
 //     connection.query("SELECT * FROM auctions.item_name", function (err, res) {
